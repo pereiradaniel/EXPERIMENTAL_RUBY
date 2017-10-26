0d9c8e0a9c6edae6f2622ca0afdb10358d6f6561
Daniel Pereira <pereiradpg@yahoo.ca>
2017-10-26 14:09:58 -0400

    Try using eval in a differente way eval() takes a strig as an argument

---
+++ RUBYMONK/RUBY_ASCENT/EVAL_02.rb
@@ -0,0 +1,8 @@
+# Creates variable answer with value of zero
+	answer = 0
+
+# Returns true after comparing answer to see if it is less than answer + 1
+	puts answer < eval("answer + 1")
+
+# Returns true after comparing answer to see if it is less than answer - 10:
+	puts answer < eval("answer - 10")
