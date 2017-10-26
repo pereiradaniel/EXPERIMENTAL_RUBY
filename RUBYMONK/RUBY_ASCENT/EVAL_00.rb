ef95a93d0aa08337899ec6e588a761394ed737ac
Daniel Pereira <pereiradpg@yahoo.ca>
2017-10-26 12:54:01 -0400

    Add example #2 and notes Second example is same as first

--- RUBYMONK/RUBY_ASCENT/EVAL_00.rb
+++ RUBYMONK/RUBY_ASCENT/EVAL_00.rb
@@ -1,4 +1,13 @@
-# Evaluates 7 * 6
-zen = eval("7 * 6")
-# Output is 42
-puts zen
+# EVAL 0.1
+
+  # EXAMPLE 1
+    # Evaluates 7 * 6
+      zen = eval("7 * 6")
+    # Output is 42
+      puts zen
+
+  # EXAMPLE 2
+    # Evaluates 7 * 6
+      zen = 7 * 6
+    # Output is 42 (same as using eval in example 1)
+      puts zen
